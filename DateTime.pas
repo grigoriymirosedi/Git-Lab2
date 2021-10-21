e217ff73b8611d414e9b9e0ead551cf25e545312
Grigoriy <grigoriymirosedi@gmail.com>
2021-10-21 10:53:58 +0300

    Added function IsLeapYear

---
+++ DateTime.pas
@@ -0,0 +1,11 @@
+﻿function IsLeapYear(year: integer) : boolean;
+begin
+  if (year mod 4 = 0) and (year mod 100 <> 0) or (year mod 400 = 0) then
+    result := True
+  else
+    result := False;
+end;
+
+begin
+  
+end.
